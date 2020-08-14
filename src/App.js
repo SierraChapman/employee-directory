33ca08670e56066709f6477a4789b210f702db5d
Sierra Chapman <siechap@gmail.com>
2020-08-13 22:00:19 -0700

    initialize state with useReducer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+const employees = require("./db/employee.json");
 
 function App() {
+
+  // set up state to keep track of display
+  const [state, dispatch] = useReducer((state, action) => {
+    return state;
+  }, {
+    employees: employees.map(employee => ({...employee, display: true})), // employee list, initialized with all displayed
+    filter: "", // string typed in search bar
+    sortedBy: "None", // used to determine which arrow to highlight
+    descending: false, // also used to determine which arrow to highlight
+  });
+
   return (
     <h1>Employee Directory</h1>
   );
