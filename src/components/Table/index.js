758bf8b942dc05c268ede329805ff3e9926b981c
Sierra Chapman <siechap@gmail.com>
2020-08-14 17:14:36 -0700

    style table spacing

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -5,18 +5,20 @@ import "./style.css";
 function Table(props) {
 
   return (
-    <table>
-      <thead>
-        <tr>
-          {props.columns.map(columnName => <th key={columnName}>{columnName}</th>)}
-        </tr>
-      </thead>
-      <tbody>
-        {props.state.employees.map(employee => {
-          return <Row data={employee} columns={props.columns} key={employee.name}/>;
-        })}
-      </tbody>
-    </table>
+    <div className="table-container">
+      <table>
+        <thead>
+          <tr>
+            {props.columns.map(columnName => <th key={columnName}>{columnName}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {props.state.employees.map(employee => {
+            return <Row data={employee} columns={props.columns} key={employee.name}/>;
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
